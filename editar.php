490742535d0587f7d4f5ad3c94b469141e889d37
michaell.mahan <mmahan13@gmail.com>
2021-01-20 11:54:17 +0100

    optimizacion del codigo solo una llamada a la bbdd

--- editar.php
+++ editar.php
@@ -2,71 +2,68 @@
     session_start();
     include('connection.php'); 
 
-    if(isset($_POST['id'])){
-        $_SESSION['id'] = $_POST['id'];
-    }
-
-    if(isset($_POST['cancelButton']))
+    if(isset($_POST['id']))
     {
-       
-        header('Location: listado.php');
-        exit;
-    }
- 
-    if(isset($_POST['SubmitButton']))
-    {
-        
-        if(!empty($_POST['shortname']) || !empty($_POST['name'])) 
-        {
-         
-            $sql = "UPDATE dwes.products SET shortname=:shortname, name=:name, descriptions=:descriptions,pvp=:pvp WHERE id=:id";
-            $query = $dwes->prepare($sql);
-            $query->bindparam(':id', $_SESSION['id']);
-            $query->bindparam(':shortname', $_POST['shortname']);
-            $query->bindparam(':name', $_POST['name']);
-            $query->bindparam(':descriptions', $_POST['descriptions']);
-            $query->bindparam(':pvp', $_POST['pvp']);
-            $query->execute();
-            
-            //se destruye la sesion
-            session_destroy();
-
-            header('Location: actualizar.php');
-            exit;
-        }else{
-          
-                //creo un array de productos 
-                $producto = array();
-                                
-                //select a la bbdd
-                $products = $dwes->query("SELECT * FROM dwes.products  WHERE id='".$_SESSION['id']."'");
-                while ($producto = $products->fetch(PDO::FETCH_ASSOC)) {
-                
-                $shortname = $producto['shortname'];
-                $name = $producto['name'];
-                $descriptions = $producto['descriptions'];
-                $pvp = $producto['pvp'];
-
-                }
-            }
-      
-    }else{
-
         //creo un array de productos 
         $producto = array();
                         
         //select a la bbdd
-        $products = $dwes->query("SELECT * FROM dwes.products  WHERE id='".$_SESSION['id']."'");
-        while ($producto = $products->fetch(PDO::FETCH_ASSOC)) {
-        
-        $shortname = $producto['shortname'];
-        $name = $producto['name'];
-        $descriptions = $producto['descriptions'];
-        $pvp = $producto['pvp'];
-
+        $products = $dwes->query("SELECT * FROM dwes.products  WHERE id='".$_POST['id']."'");
+        while ($producto = $products->fetch(PDO::FETCH_ASSOC)) 
+        {
+            $_SESSION['data'] = [
+                'id' => $_POST['id'],
+                'shortname' => $producto['shortname'],
+                'name' => $producto['name'],
+                'descriptions' => $producto['descriptions'],
+                'pvp' => $producto['pvp']
+            ];
         }
     }
-   
+    
+    //al presionar el boton cancelar redirijo al listado
+    if(isset($_POST['cancelButton']))
+    { 
+        session_destroy();
+        header('Location: listado.php');
+        exit;
+    }
+    
+    //al presionar el boton actualiar
+    if(isset($_POST['SubmitButton']))
+    {  
+        if(!empty($_POST['shortname']))
+        {
+            if(strlen($_POST['shortname']) <= 45)
+            {
+                if(!empty($_POST['name']))
+                {
+                    if(strlen($_POST['name']) <= 60)
+                    {
+                        if(!empty($_POST['pvp']))
+                        {
+                            //Si todoas las condiciones se cumplen hacemos el Update a la bbdd
+                            $sql = "UPDATE dwes.products SET shortname=:shortname, name=:name, descriptions=:descriptions,pvp=:pvp WHERE id=:id";
+                            $query = $dwes->prepare($sql);
+                            $query->bindparam(':id', $_SESSION['data']['id']);
+                            $query->bindparam(':shortname', $_POST['shortname']);
+                            $query->bindparam(':name', $_POST['name']);
+                            $query->bindparam(':descriptions', $_POST['descriptions']);
+                            $query->bindparam(':pvp', $_POST['pvp']);
+                            $query->execute();
+                            
+                            //se destruye la sesion
+                            session_destroy();
+                            
+                            //redirijo a actualiar.php
+                            header('Location: actualizar.php');
+                            exit;
+                        }
+                    }
+                }
+            }
+        }   
+    }
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -87,23 +84,34 @@
 <div id="contenido">
     <h2>Producto:</h2>
     <form  method='post'>
-        Nombre corto:<input type='text' name='shortname' value="<?php echo $shortname; ?>">
+        Nombre corto:<input type='text' name='shortname' value="<?php echo $_SESSION['data']['shortname']; ?>">
         <?php
                if (isset($_POST['SubmitButton']) && empty($_POST['shortname']))
                 echo "<p style='color:red'>El campo nombre corto es obligatorio</p>";
+
+                
+                if (isset($_POST['SubmitButton']) && strlen($_POST['shortname']) >= 45)
+                echo "<p style='color:red'>El campo nombre corto es muy largo</p>";
           ?>
         <br><br>
         Nombre:<br>
-        <textarea name="name" value="" rows="5" cols="50"><?php echo $name; ?></textarea>
+        <textarea name="name" value="" rows="5" cols="50"><?php echo $_SESSION['data']['name']; ?></textarea>
         <?php
                if (isset($_POST['SubmitButton']) && empty($_POST['name']))
                 echo "<p style='color:red'>El campo nombre es obligatorio</p>";
+
+                if (isset($_POST['SubmitButton']) && strlen($_POST['name']) >= 60)
+                echo "<p style='color:red'>El campo nombre es muy largo</p>";
           ?>
         <br><br>
         Descripción:<br>
-        <textarea name="descriptions" rows="7" cols="50"><?php echo $descriptions; ?></textarea>
+        <textarea name="descriptions" rows="7" cols="50"><?php echo $_SESSION['data']['descriptions']; ?></textarea>
         <br><br>
-        PVP:<input type='text' name='pvp' value="<?php echo $pvp; ?>">
+        PVP:<input type='text' name='pvp' value="<?php echo $_SESSION['data']['pvp']; ?>">
+        <?php
+               if (isset($_POST['SubmitButton']) && empty($_POST['pvp']))
+                echo "<p style='color:red'>El campo pvp es obligatorio</p>";
+          ?>
         <br><br><br>
         <button type="submit" name="SubmitButton">Actualizar</button>
         <button type="submit" name="cancelButton">Cancelar</button>
